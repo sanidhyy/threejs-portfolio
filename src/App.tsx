62a3bd337df9ea98d9dd5bb17d937227d3d5dba1
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-24 07:28:25 +0000

    tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>App</div>;
+  return (
+    <main className="max-w-7xl mx-auto">
+      <h1 className="text-2xl text-white underline">Hello, World!</h1>
+    </main>
+  );
 }
 
 export default App;
