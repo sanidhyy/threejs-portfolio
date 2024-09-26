9f84ef2252ec104790fd92fa58bed9eda10f8d8a
Sanidhya Kumar Verma <sanidhya.verma12345@gmail.com>
2024-09-26 04:12:24 +0000

    env types added and gitignore updated

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
+// This file is needed to support autocomplete for import.meta.env
+
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_APP_SERVICE_ID: string;
+  readonly VITE_APP_TEMPLATE_ID: string;
+  readonly VITE_APP_EMAIL: string;
+  readonly VITE_APP_PUBLIC_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
